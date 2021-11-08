57f73ae35d66e4d2301b202d6050f642e5c293ae
CristianTura <cristianbuenaventura15@gmail.com>
2021-11-08 14:46:31 -0500

    08-11-21 | config css | cb

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,5 +1,9 @@
-import React from 'react';
+import React from "react";
+import Footer from "../components/footer/Footer";
+import "../assets/styles/global2.css";
 
-const App = () => <h1>Hello React!!!</h1>
+const App = () => {
+    return <Footer />;
+};
 
 export default App;
