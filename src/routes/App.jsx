77ffff65b66a77fd67711652ce774215d7f320b3
diomedes1233 <83196146+diomedes1233@users.noreply.github.com>
2021-11-11 14:51:50 -0500

    11-11-2021 Maquetación EDB

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
-import Form_PersonalInfo from '../components/formInfo/Form_PersonalInfo'
 import "../assets/styles/global2.css"
 import Ofertas_laborales from '../components/ofertas_laborales/Ofertas_laborales'
 
 
 const App = () => 
 {return(
-<Form_PersonalInfo/>,
+
 <Ofertas_laborales/>
 )}
 
