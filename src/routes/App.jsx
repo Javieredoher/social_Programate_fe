19038f1086c9f7fa6d05a8e14db1a461e9b792ea
diomedes1233 <83196146+diomedes1233@users.noreply.github.com>
2021-11-11 11:18:52 -0500

    11-11-2021 Maquetacion formulario EDB

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import Form_PersonalInfo from '../components/formInfo/Form_PersonalInfo'
 import "../assets/styles/global2.css"
-import Ofertas_laborales from '../components/ofertas_laborales/Ofertas_laborales'
+
 
 
 const App = () => 
 {return(
-<Form_PersonalInfo/>,
-<Ofertas_laborales/>
+<Form_PersonalInfo/>
 )}
 
 export default App
