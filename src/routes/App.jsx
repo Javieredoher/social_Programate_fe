a34f22227dcf62b417ecef6ff20b5072e7d3daaa
James Efrain Rodriguez Fonseca <jamesrodriguez3188@gmail.com>
2021-11-12 09:36:07 -0500

    11-12-21|formulario eventos|JERF

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import Footer from "../components/footer/Footer";
 import "../assets/styles/global2.css";
+import FormEvent from "../components/FormEvent/FormEvent";
 
 const App = () => {
-    return <Footer />;
+    return <FormEvent />;
 };
 
 export default App;
