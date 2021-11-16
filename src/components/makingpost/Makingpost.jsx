7198625547928f40a8f02149871fa77b6f0d4069
AdrianPC <adrianchooo18@gmail.com>
2021-11-16 15:57:34 -0500

    created post finished

--- src/components/makingpost/Makingpost.jsx
+++ src/components/makingpost/Makingpost.jsx
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { Fragment } from "react";
 import styles from "../makingpost/Makingpost.module.css";
 
 const Makingpost = () => {
     return (
-        <div>
-            <h1>Soy una prueba</h1>
-        </div>
-    )
+        <Fragment>
+            <div className={styles.card_container}>
+                <div className={styles.card}>
+                <i class="bx bxs-user"></i>
+                <select name="" id="post" className={styles.share}>
+                    <option value="">Crear públicacion</option>
+                    <option value="comentario">Añadir comentario</option>
+                    <option value="documento">Añadir documento</option>
+                    <option value="noticia">Añadir noticia</option>
+                    <option value="imagen">Añadir imagen</option>
+                </select>
+                </div>
+            </div>
+        </Fragment>
+    );
 };
 
 export default Makingpost;
