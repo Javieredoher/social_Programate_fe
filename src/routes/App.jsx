64a6fab737ef93a7a3bdd62da735da86fa279c2e
diomedes1233 <83196146+diomedes1233@users.noreply.github.com>
2021-11-16 15:13:09 -0500

    updated

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 // import "../assets/styles/global2.css"
 import Lista_Usuarios from '../components/lista_usuarios/Lista_Usuarios'
+import Ofertas_laborales from '../components/ofertas_laborales/Ofertas_laborales'
 
 
 
 
 const App = () => 
 {return(
-<Lista_Usuarios/>
+
+<Ofertas_laborales/>
 
 )}
 
