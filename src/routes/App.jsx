20bf85ee1f353faa4436a26a60339b67e2552bb8
diomedes1233 <83196146+diomedes1233@users.noreply.github.com>
2021-11-16 15:39:15 -0500

    16-11-2021 compile views EDB

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import Form_PersonalInfo from '../components/formInfo/Form_PersonalInfo'
 import "../assets/styles/global2.css"
+import Ofertas_laborales from '../components/ofertas_laborales/Ofertas_laborales'
+import Lista_Usuarios from '../components/lista_usuarios/Lista_Usuarios'
 
 
 
@@ -8,7 +10,7 @@ import "../assets/styles/global2.css"
 
 const App = () => 
 {return(
-<Form_PersonalInfo/>
+<Lista_Usuarios/>
 )}
 
 export default App
