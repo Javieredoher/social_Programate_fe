8fc05ecb393d78c4dfbf33d6a55fa4378dbd5a52
diomedes1233 <83196146+diomedes1233@users.noreply.github.com>
2021-11-16 14:39:35 -0500

    16-11-2021 EDB

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
-import "../assets/styles/global2.css"
-import Ofertas_laborales from '../components/ofertas_laborales/Ofertas_laborales'
+// import "../assets/styles/global2.css"
+import Lista_Usuarios from '../components/lista_usuarios/Lista_Usuarios'
+
+
 
 
 const App = () => 
 {return(
+<Lista_Usuarios/>
 
-<Ofertas_laborales/>
 )}
 
 export default App
