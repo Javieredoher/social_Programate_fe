7126ab420ffe2e41ed713ba90f419602b72aa13b
AdrianPC <adrianchooo18@gmail.com>
2021-11-17 17:53:08 -0500

    17-11-21 |correction responsive width|ACPC

--- src/components/commentbox/Commentbox.jsx
+++ src/components/commentbox/Commentbox.jsx
@@ -1,20 +1,26 @@
-import React, {Fragment} from 'react';
-import styles from './Commentbox.module.css';
+import React, { Fragment } from "react";
+import styles from "./Commentbox.module.css";
 
 const Commentbox = () => {
     return (
         <Fragment>
             <div className={styles.card_container}>
                 <div className={styles.card}>
-                <i class="bx bxs-user"></i>
-                <input type="text" className={styles.input_box} placeholder="Añadir un comentario"/>
-                </div>
-                <div className={styles.button_box}>
-                <button type="submit" className={styles.btn}>Públicar</button>
+                    <i class="bx bxs-user"></i>
+                    <input
+                        type="text"
+                        className={styles.input_box}
+                        placeholder="Añadir un comentario"
+                    />
+                    <div className={styles.button_box}>
+                        <button type="submit" className={styles.btn}>
+                            Publicar
+                        </button>
+                    </div>
                 </div>
             </div>
         </Fragment>
-    )
+    );
 };
 
 export default Commentbox;
