f14121789b2ec5fd4086ef6d2f3dc7a45fb91d41
diomedes1233 <83196146+diomedes1233@users.noreply.github.com>
2021-11-19 11:49:12 -0500

    19-11-2021 se termino la vista perfil EDB

---
+++ src/components/Posts/Posts.jsx
@@ -0,0 +1,31 @@
+import React, { Fragment } from 'react'
+
+import style from './Posts.module.css'
+
+const Posts = () => {
+    return (
+        <Fragment>
+            <secction className={style.container1}>
+                <div className={style.container2}>
+
+                    <div className={style.icon_cont1}>
+                        <div className={style.icon}><i class="far fa-user-circle"></i></div>
+                        <p><b>Juan Hernando Fernandez</b><br />QuakCoders<br /> <span>2 hr</span></p>
+
+                    </div>
+                    <div>
+                        <p> <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi accusantium ipsam explicabo saepe, dolorum quo accusamus, quisquam nihil recusandae optio possimus aliquam deleniti, aspernatur error! Corrupti sequi nihil officiis molestiae?</p></p>
+                    </div>
+                    <div className={style.icon_cont2}>
+                    <div><i class="far fa-thumbs-up"></i><span>23</span></div>
+                    <div><i class="far fa-comment-dots"></i></div>
+                    <div><i class="fas fa-share"></i></div>
+                    </div>
+                </div>
+            </secction>
+
+        </Fragment>
+    )
+}
+
+export default Posts
--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,13 +1,29 @@
 import React from 'react'
 import "../assets/styles/global2.css"
-import EditarProyectos from '../components/editarproyectos/EditarProyectos'
+import ProfileAbout from '../components/ProfileAbout/ProfileAbout'
+
+import Posts from '../components/Posts/Posts'
+import ProfileLanguages from '../components/ProfileLanguages/ProfileLanguages'
+import Technologies from '../components/Technologies/Technologies'
+import ProfileEducation from '../components/ProfileEducation/ProfileEducation'
+import ProfileMain from '../components/ProfileMain/ProfileMain'
+import ProfileSkiles from '../components/ProfileSkiles/ProfileSkiles'
 
 
 
-const App = () => 
-{return(
+const App = () => {
+    return (
+        <div>
+            <ProfileMain/>,
+            <ProfileAbout/>,
+            <ProfileSkiles/>,
+            <ProfileEducation/>,
+            <Technologies/>,
+            <ProfileLanguages/>,
+            <Posts/>
+        </div>
 
-<EditarProyectos/>
-)}
+    )
+}
 
 export default App
