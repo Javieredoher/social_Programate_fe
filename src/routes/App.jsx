f67b0a1d5b783bd91d93a555de3cd10788f0b0aa
diomedes1233 <83196146+diomedes1233@users.noreply.github.com>
2021-11-19 21:39:04 -0500

    19-11-2021 se termino la vista LatestPosts y la adminHome EDB

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,64 +1,15 @@
 import React from "react";
 import "../assets/styles/global2.css";
-import { DataProvider } from "../context/DataContext";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import CompletePerfil from "../pages/CompletePerfil";
-import FormNewsPage from "../pages/FormNewsPage";
-import FormEventPage from "../pages/FormEventPage";
-import Start from "../components/Start";
-import FormJobsPage from "../pages/FormJobsPage";
-import CommunityPage from "../pages/CommunityPage";
-import PortfolioPage from '../components/portfolio/Portfolio'
-import FormProject from '../components/FormProject/FormProject'
+
+import LatestPosts from "../components/LatestPosts/LatestPosts.jsx";
 
 
 const App = () => {
     return (
-        <DataProvider>
-            <Router>      
-                <Switch>
-      
-                    <Route exact path="/">
-                        <Start />
-                    </Route>
-      
-                    <Route exact path="/formprofile">
-                        <CompletePerfil />
-                    </Route>
-
-
-                    <Route exact path="/formevent">
-                        <FormEventPage />
-                    </Route>
-
-                    <Route exact path="/formnews">
-                        <FormNewsPage />
-                    </Route>
-
-                    <Route exact path="/formnews/:id">
-                        <FormNewsPage />
-                    </Route>
-
-                    <Route exact path="/formjobs">
-                        <FormJobsPage />
-                    </Route>
-
-                    <Route exact path="/community">
-                        <CommunityPage />
-                    </Route>
-
-                    <Route exact path="/portfolio">
-                        <PortfolioPage />
-                    </Route>
-
-                    <Route exact path="/formproject">
-                        <FormProject />
-                    </Route>
-
-
-                </Switch>
-            </Router>
-        </DataProvider>
+        <div>
+            <LatestPosts/>
+           
+        </div>
 
     );
 };
