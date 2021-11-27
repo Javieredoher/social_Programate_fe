9da19083454927780e691244782792f326293006
Rafael Ramírez <rrramirezb251@gmail.com>
2021-11-26 21:20:24 -0500

    26-11-21|comments home added|RRRB

--- src/components/homePublications/Events.jsx
+++ src/components/homePublications/Events.jsx
@@ -1,7 +1,8 @@
 import React, { useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { DataContext } from "../../context/DataContext";
-import { deleteData, getData, getDataAll } from "../../helpers/fetch";
+import { deleteData, getData, getDataAll, sendData } from "../../helpers/fetch";
+import styles from "./Comment_likes.module.css";
 
 import style from "./Posts.module.css";
 
@@ -16,19 +17,108 @@ const News = ({
     user,
 }) => {
     const { setGetPosts, idUser } = useContext(DataContext);
+    const [showComments, setShowComments] = useState(false);
+    const [moreComments, setMoreComments] = useState(false);
+    const [inputComment, setInputComment] = useState("")
+    const [postComments, setPostComments] = useState([]);
+    const [comments, setComments] = useState([]);
+    const [users, setUsers] = useState([]);
+    const [refresh, setRefresh] = useState(false);
+    const [userPost, setUserPost] = useState();
 
     let navigate = useNavigate();
 
-    const [userPost, setUserPost] = useState();
+    const commentInfo = async () => {
+        const data = await getData("posts", id);
 
-    useEffect(async () => {
+        setComments(comments => data.comments);
+
+    }
+    const getUsers = async () => {
+        const data = await getDataAll(`users`);
+        setUsers(data);
+    }
+    const getUser = async () => {
         try {
             const data = await getData("users", user);
             setUserPost(data);
         } catch (error) {
             console.log(error);
         }
+    }
+
+    useEffect(() => {
+        getUser();
+        commentInfo()
+        getUsers();
+
     }, []);
+    useEffect(() => {
+        commentInfo();
+    }, [setRefresh, refresh]);
+    const submitData = async (e) => {
+        e.preventDefault();
+
+        try {
+            await sendData(`posts/comment/${id}`, postComments);
+            setInputComment("")
+            setRefresh(!refresh)
+        } catch (error) {
+            console.log("Error" + error);
+        }
+    };
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setInputComment(value)
+        setPostComments({ ...postComments, [name]: value, user_id: idUser })
+
+    };
+
+    const toggle = () => {
+        console.log("click");
+        setShowComments(!showComments)
+    }
+    const onName = (id) => {
+        const user = users.filter(user => user._id === id)
+        const userFilter = user[0];
+        console.log(userFilter);
+        return `${userFilter.firstName} ${userFilter.lastName}`
+    }
+
+    const onImage = (id) => {
+        const user = users.filter(user => user._id === id)
+        const userFilter = user[0];
+        return userFilter.avatar
+    }
+
+    const onDelete = async (id) => {
+        await deleteData('comments', id)
+        setRefresh(!refresh)
+
+
+    }
+
+    const previewComment = (comment, index) => {
+        return (
+            <div key={index} className={styles.comments}>
+                <div className={styles.comment_div}>
+                    <div className={styles.header}>
+                        <img className={styles.img} src={onImage(comment.user_id)} alt={comment.user_id} />
+                        <span className={styles.name}>{onName(comment.user_id)}</span>
+
+                    </div>
+
+                    <span>{comment.comment}</span>
+                    {comment.user_id === idUser &&
+                        <span onClick={() => onDelete(comment._id)} className={styles.delete} >Eliminar
+                        </span>}
+                </div>
+
+            </div>
+        )
+    }
+
 
     const deletePost = async () => {
         try {
@@ -97,13 +187,37 @@ const News = ({
                         <span>23</span>
                     </div>
                     <div className={style.like}>
-                        <i className="far fa-comment-dots"></i>
-                        <span>20</span>
+                        <i onClick={toggle} className="far fa-comment-dots"></i>
+                        <span>{comments.length}</span>
                     </div>
                     <div>
                         <i className="fas fa-share"></i>
                     </div>
                 </div>
+                {showComments &&
+
+                    <div>
+                        <p>deja tu comentario</p>
+                        <form className={styles.form} onSubmit={submitData}>
+                            <textarea name="comment" value={inputComment} className={styles.input_comment} onChange={handleChange} ></textarea>
+                            <button className={styles.submit}>Enviar</button>
+                        </form>
+                        {comments.map((comment, index) => (
+                            index < 2 && previewComment(comment, index)
+                        ))}
+
+                        <span className={styles.more_comment} onClick={() => setMoreComments(!moreComments)}>Ver más comentarios</span>
+                        {moreComments &&
+                            comments.map((comment, index) => (
+                                index >= 2 &&
+                                previewComment(comment, index)
+                            ))
+                        }
+
+
+                    </div>
+
+                }
             </div>
         </section>
     );
--- src/components/homePublications/News.jsx
+++ src/components/homePublications/News.jsx
@@ -1,25 +1,55 @@
 import React, { useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { DataContext } from "../../context/DataContext";
-import { deleteData, getData, getDataAll } from "../../helpers/fetch";
+import { deleteData, getData, getDataAll, sendData } from "../../helpers/fetch";
 import style from "./Posts.module.css";
+import styles from "./Comment_likes.module.css";
 import Technologies from "./Technologies";
+
 
 const News = ({ description, images, technologies, title, id, user }) => {
     const { setGetPosts, idUser } = useContext(DataContext);
+    const [showComments, setShowComments] = useState(false);
+    const [moreComments, setMoreComments] = useState(false);
+    const [inputComment, setInputComment] = useState("")
+    const [postComments, setPostComments] = useState([]);
+    const [comments, setComments] = useState([]);
+    const [users, setUsers] = useState([]);
+    const [refresh, setRefresh] = useState(false);
 
     let navigate = useNavigate();
 
     const [userPost, setUserPost] = useState();
+    const commentInfo = async () => {
+        const data = await getData("posts", id);
 
-    useEffect(async () => {
+        setComments(comments => data.comments);
+
+    }
+    const getUsers = async () => {
+        const data = await getDataAll(`users`);
+        setUsers(data);
+    }
+    const getUser = async () => {
         try {
             const data = await getData("users", user);
             setUserPost(data);
         } catch (error) {
             console.log(error);
         }
+    }
+
+    useEffect(() => {
+        getUser();
+        commentInfo()
+        getUsers();
+
     }, []);
+    useEffect(() => {
+        commentInfo()
+    }, [refresh, setRefresh])
+
+
 
     const deletePost = async () => {
         try {
@@ -30,6 +60,71 @@ const News = ({ description, images, technologies, title, id, user }) => {
             console.log(error);
         }
     };
+
+    const toggle = () => {
+        console.log("click");
+        setShowComments(!showComments)
+    }
+    const submitData = async (e) => {
+        e.preventDefault();
+
+        try {
+            await sendData(`posts/comment/${id}`, postComments);
+            setInputComment("")
+            setRefresh(!refresh)
+        } catch (error) {
+            console.log("Error" + error);
+        }
+    };
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setInputComment(value)
+        setPostComments({ ...postComments, [name]: value, user_id: idUser })
+
+    };
+
+
+    const onName = (id) => {
+        const user = users.filter(user => user._id === id)
+        const userFilter = user[0];
+        console.log(userFilter);
+        return `${userFilter.firstName} ${userFilter.lastName}`
+    }
+
+    const onImage = (id) => {
+        const user = users.filter(user => user._id === id)
+        const userFilter = user[0];
+        return userFilter.avatar
+    }
+
+    const onDelete = async (id) => {
+        await deleteData('comments', id)
+        setRefresh(!refresh)
+
+
+    }
+
+    const previewComment = (comment, index) => {
+        return (
+            <div key={index} className={styles.comments}>
+                <div className={styles.comment_div}>
+                    <div className={styles.header}>
+                        <img className={styles.img} src={onImage(comment.user_id)} alt={comment.user_id} />
+                        <span className={styles.name}>{onName(comment.user_id)}</span>
+
+                    </div>
+
+                    <span>{comment.comment}</span>
+                    {comment.user_id === idUser &&
+                        <span onClick={() => onDelete(comment._id)} className={styles.delete} >Eliminar
+                        </span>}
+                </div>
+
+            </div>
+        )
+    }
+
 
     return (
         <section className={style.container1}>
@@ -83,13 +178,39 @@ const News = ({ description, images, technologies, title, id, user }) => {
                         <span>23</span>
                     </div>
                     <div className={style.like}>
-                        <i className="far fa-comment-dots"></i>
-                        <span>20</span>
+                        <i onClick={toggle} className="far fa-comment-dots"></i>
+                        <span>{comments.length}</span>
+
                     </div>
+
                     <div>
                         <i className="fas fa-share"></i>
                     </div>
                 </div>
+                {showComments &&
+
+                    <div>
+                        <p>deja tu comentario</p>
+                        <form className={styles.form} onSubmit={submitData}>
+                            <textarea name="comment" value={inputComment} className={styles.input_comment} onChange={handleChange} ></textarea>
+                            <button className={styles.submit}>Enviar</button>
+                        </form>
+                        {comments.map((comment, index) => (
+                            index < 2 && previewComment(comment, index)
+                        ))}
+
+                        <span className={styles.more_comment} onClick={() => setMoreComments(!moreComments)}>Ver más comentarios</span>
+                        {moreComments &&
+                            comments.map((comment, index) => (
+                                index >= 2 &&
+                                previewComment(comment, index)
+                            ))
+                        }
+
+
+                    </div>
+
+                }
             </div>
         </section>
     );
