6f7b1626afcf0fcb1df1bc646944b05b9b9a2537
julianamrg <jumeroga1@gmail.com>
2021-11-26 23:34:56 -0500

    26-11 adding navbar and footer in forum pages JMR

--- src/components/ForumQuestions/Search.jsx
+++ src/components/ForumQuestions/Search.jsx
@@ -2,32 +2,32 @@ import React from "react";
 import styles from "./ForumQuestions.module.css";
 import { BiSearch } from "react-icons/bi";
 import { useState } from "react";
-import { useNavigate } from "react-router";
+import { useNavigate } from "react-router-dom";
 
 export const Search = () => {
-  const [searchText, setSearchText] = useState("");
+    const [searchText, setSearchText] = useState("");
 
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+    const handleSubmit = (e) => {
+        e.preventDefault();
         navigate("/questions?search=" + searchText);
-  };
+    };
 
-  return (
-    <form className={styles.searchContainer} onSubmit={handleSubmit}>
-      <div className={styles.searchBox}>
-        <input
-          type="text"
-          placeholder="Buscar pregunta"
-          value={searchText}
-          onChange={(e) => setSearchText(e.target.value)}
-          className={styles.searchInput}
-        />
-        <button type="submit" className={styles.searchButton}>
-          <BiSearch size="20" />
-        </button>
-      </div>
-    </form>
-  );
+    return (
+        <form className={styles.searchContainer} onSubmit={handleSubmit}>
+            <div className={styles.searchBox}>
+                <input
+                    type="text"
+                    placeholder="Buscar pregunta"
+                    value={searchText}
+                    onChange={(e) => setSearchText(e.target.value)}
+                    className={styles.searchInput}
+                />
+                <button type="submit" className={styles.searchButton}>
+                    <BiSearch size="20" />
+                </button>
+            </div>
+        </form>
+    );
 };
